2ddbe3c018d0a0c86984a8a7f3fba2a5820c1908
Robert <robertbaddeley@gmail.com>
2021-04-24 15:59:14 +0900

    redesigned nav bar to be on side/bottom responsive

---
+++ components/navbar/NavItem.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Navbar.module.css'
+
+interface Props {
+    icon: any;
+    label: string;
+    url: string;
+}
+
+const NavItem = ({icon, label, url}: Props) => {
+    return (
+        <li className={styles.navItem}>
+            <Link href={url}>
+                <a className={styles.navLink}>
+                    <div className={styles.icon}>
+                        {icon}
+                    </div>
+                    <span className={styles.linkText}>
+                        {label}
+                    </span>
+                </a>
+            </Link>
+        </li>
+    )
+}
+
+export default NavItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
 
 interface Props {
   
@@ -8,9 +9,10 @@ interface Props {
 
 const Home = (props: Props) => {
   return (
-    <div>
-      <Loader show/>
-    </div>
+    <main>
+      <Loader show={false}/>
+      <button onClick={() => toast.success('toast message test')} >Toast it</button>
+    </main>
   )
 }
 
