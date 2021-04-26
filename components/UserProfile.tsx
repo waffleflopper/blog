b4a918efa9c0372bb2f1bbe34194b2d5de545b92
Robert <robertbaddeley@gmail.com>
2021-04-26 23:48:12 +0900

    index page post listing

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -9,25 +9,17 @@ interface Props {
 
 const UserProfile = ({ user }: Props) => {
   return (
-    <div className={styles.header}>
-      <img className={styles.profileImage} src={user.photoURL} />
-      <span className={styles.userInfo}>
-        <h1>@{user.username}</h1>
-        <p>{user.displayName}</p>
-      </span>
-    </div>
+    (user &&
+      <div className={styles.header}>
+        <img className={styles.profileImage} src={user.photoURL} />
+        <span className={styles.userInfo}>
+          <h1>@{user.username}</h1>
+          <p>{user.displayName}</p>
+        </span>
+      </div>
+    )
   );
 };
 
 export default UserProfile;
 
-/*
-
-<span class="header">
-      <image class="profileImage" src="https://lh3.googleusercontent.com/a-/AOh14Gi9sN70DeRU4rxzyrUNP0zqrTOCEAk2hNa5_xvZ4w=s96-c">
-      <span class="userInfo">
-        <h1>@username</h1>
-        <p>Full Name</p>
-      </span>
-    </span>
-*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "../components/Loader";
 import toast from "react-hot-toast";
-import Link from "next/link";
+import { firestore, postToJSON, fromMillis } from "../lib/firebase";
+import PostFeed from '../components/PostFeed';
 
-interface Props {}
+
+interface Props {
+  posts: any;
+}
+
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postQuery = firestore
+    .collectionGroup('posts')
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  }
+}
 
 const Home = (props: Props) => {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+
+    setLoading(true);
+
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((docs) => docs.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  }
+
+
   return (
     <main>
-      <Loader show={false} />
-      <button onClick={() => toast.success("toast message test")}>
-        Toast it
-      </button>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load More</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && <p>No more posts</p>}
     </main>
   );
 };
